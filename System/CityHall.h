5d629e7a4951d1fe51580f08ed0ce339b4411178
denis-woolley <u23528860@tuks.co.za>
2024-10-29 21:00:58 +0200

    Updated CityHall

--- System/CityHall.h
+++ System/CityHall.h
@@ -2,36 +2,51 @@
 #define CITYHALL_H
 
 #include "SatisfactionHandler.h"
+#include "PoliticalSystem.h"
 #include "CityMap.h"
  
 class CityHall{
     private:
         static CityHall* cityHall; // Singleton
-        //PoliticalSystem* politicalSystem; (CAN'T DO THIS ONE YET)
         SatisfactionHandler* chainOfResponsibility;
-        CityMap* cityMapReference;
+        PoliticalSystem* politicalSystem;
 
-        int currPopulation;
+        int numCitizens;
         int popeCoins;
-        int citySatisfaction;
-        int chainHandleCounter;
-        int chainHandleCounterMax;
+        float citySatisfaction;
+        int cityCapacity;
+        int numResidentialBuildings;
+        float residentialSatisfaction;
+
+        int taxRateResidential;
+        int taxRateCommercial;
+        int taxRateIndustrial;
+        double budgetSplit;
+        int citizenSatisfactionImpact;
     protected:
         CityHall(); // Singleton
     public:
         static CityHall* getInstance(); // Singleton
-        //void setCityMapReference(CityMap* map); (CAN'T DO THIS ONE YET)
+        void setPoliticalSystem(PoliticalSystem* politicalSystem);
         void birth();
         void death();
         void emigrate();
         void immigrate();
-        int getCityCapacity();
-        int getCurrNumCitizens();
-        int getCurrSatisfaction(); // Request to chain of responsibility
-        void calculateCitySatisfaction();
-        void calculateHousedSatisfaction();
+
+        int getCurrSatisfaction();
+        float calculateSatisfaction();
+        void calculateHousedSatisfaction(); // Mediator can do this (this is the calculation we were talking about Vansh)
+        float getRailwayBonus();
+        float getAirportBonus();
         void calculateHomelessness();
         int calculatePopeCoins();
+
+        int getTaxRateResidential();
+        int getTaxRateCommercial();
+        int getTaxRateIndustrial();
+        double getBudgetSplit(); // Higher the percentage the more money goes to the user
+        int getCitizenSatisfactionImpact();
+        ~CityHall();
 };
 
 #endif
