76abb1931cb2db96118e91ec133104142100aa01
Vansh Sood <vanshsood4@gmail.com>
2024-10-31 16:43:17 +0200

    Started main

--- System/main.cpp
+++ System/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 
+#include "CityMap.h"
+#include "CityHall.h"
+
+// BUILDING TYPES
+#include "CityStructure.h"  
+#include "Road.h"
+#include "ResidentialComplex.h"
+#include "ResidentialBuilding.h"
+#include "CommercialBuilding.h"
+#include "IndustrialBuilding.h"
+#include "Landmark.h"
+
+// RESIDENTIAL BUILDING TYPES
+#include "House.h"
+#include "Apartment.h"
+#include "Townhouse.h"
+#include "Estate.h"
+
+// COMMERCIAL BUILDING TYPES
+#include "Shop.h"
+#include "Mall.h"
+#include "Office.h"
+
+// INDUSTRIAL BUILDING TYPES
+#include "Plant.h"
+#include "Factory.h"
+#include "Warehouse.h"
+
+// LANDMARK TYPES
+#include "Colosseum.h"
+#include "Pantheon.h"
+#include "Park.h"
+
+
+
+
+
+
 using namespace std;
 
 // SET-UP REQUIREMENTS SO FAR:
@@ -8,6 +46,20 @@ using namespace std;
 
 int main(){
 
+    // CREATE CITY MAP
+    CityMap* cityMap = new CityMap(20, 20);
+
+    // CREATE AND PLACE ROADS
+    Road* road1 = new Road();
+    road1->placeStructure(0, 1, cityMap);
+
+    // CREATE AND PLACE CITY HALL
+    CityHall* cityHall = CityHall::getInstance();
+    cityHall->placeStructure(0, 0, cityMap);
+
+    
+
+    
   
     return 0;
 }
