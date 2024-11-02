274c7982c67e9e9afd62331bd8674b2ac403216c
Jared-Hurlimann <u23543932@tuks.co.za>
2024-11-03 00:06:04 +0200

    WastePlant changed

--- System/CityStructures/Industrial/WastePlant.h
+++ System/CityStructures/Industrial/WastePlant.h
@@ -15,3 +15,19 @@ class WastePlant : public PlantDecorator {
 };
 
 #endif
+#ifndef WastePlant_h
+#define WastePlant_h
+
+#include "PlantDecorator.h"
+class PlantDecorator;
+
+class WastePlant : public PlantDecorator {
+    public:
+        WastePlant(Plant* plant) : PlantDecorator(plant) {this->plant = plant;};
+        virtual ~WastePlant() {};
+        int getRadiusOfEffect() { return plant->getRadiusOfEffect() + 1; };
+        string getPlantType();
+        string setWastePlant();
+};
+
+#endif
