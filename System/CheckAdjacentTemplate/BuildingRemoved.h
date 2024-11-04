ccaf8880e4ad85a6a566e4624bd0613191b25889
mutarisi <147860027+mutarisi@users.noreply.github.com>
2024-11-04 22:34:40 +0200

    Add files via upload

--- System/CheckAdjacentTemplate/BuildingRemoved.h
+++ System/CheckAdjacentTemplate/BuildingRemoved.h
@@ -4,12 +4,30 @@
 #include "CheckAdjacent.h"
 #include "../CityStructures/CityStructure.h"
 
-
+/**
+ * @class BuildingRemoved
+ * @brief Represents an action that occurs when a building is removed from the map.
+ *
+ * BuildingRemoved performs an action on a 2D map by notifying the city structure
+ * at a specified location that a new building has been added at that position.
+ * This class inherits from CheckAdjacent.
+ */
 class BuildingRemoved : public CheckAdjacent {
-    public:
-        void performAction(std::vector<std::vector<CityStructure*> > map, int x, int y) {
-                map.at(y).at(x)->newBuildingWasAdded();
-        }
+public:
+    /**
+     * @brief Executes the action of marking a new building as added at a specific location.
+     *
+     * This function modifies the specified cell in the map by calling `newBuildingWasAdded()`
+     * on the `CityStructure` located at (x, y), indicating that a building was added
+     * at this position.
+     *
+     * @param map 2D vector representing the city map with `CityStructure` pointers.
+     * @param x The x-coordinate of the building's location.
+     * @param y The y-coordinate of the building's location.
+     */
+    void performAction(std::vector<std::vector<CityStructure*>> map, int x, int y) {
+        map.at(y).at(x)->newBuildingWasAdded();
+    }
 };
 
-#endif
+#endif // BUILDING_REMOVED_H
