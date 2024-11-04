d1dd5eed05d112013e39d9b0f02e7d0cec8361f5
mutarisi <147860027+mutarisi@users.noreply.github.com>
2024-11-04 22:35:42 +0200

    Add files via upload

--- System/CheckAdjacentTemplate/RoadRemoved.h
+++ System/CheckAdjacentTemplate/RoadRemoved.h
@@ -5,12 +5,28 @@
 #include "../CityStructures/CityStructure.h"
 #include "../CityMap.h"
 
-
+/**
+ * @class RoadRemoved
+ * @brief Represents an action to be performed when a road is removed from the city map.
+ *
+ * The `RoadRemoved` class provides functionality to mark a specified location on the map
+ * as having a road removed. It inherits from `CheckAdjacent` and overrides the `performAction` method.
+ */
 class RoadRemoved : public CheckAdjacent {
-    public:
-        void performAction(std::vector<std::vector<CityStructure*> > map, int x, int y) {
-                map.at(y).at(x)->roadWasRemoved();
-        }
+public:
+    /**
+     * @brief Executes the action of marking a road as removed at a specific location.
+     *
+     * This function modifies the specified cell in the map by calling `roadWasRemoved()`
+     * on the `CityStructure` located at (x, y), indicating that a road was removed from this position.
+     *
+     * @param map 2D vector representing the city map with `CityStructure` pointers.
+     * @param x The x-coordinate of the road's location.
+     * @param y The y-coordinate of the road's location.
+     */
+    void performAction(std::vector<std::vector<CityStructure*>> map, int x, int y) override {
+        map.at(y).at(x)->roadWasRemoved();
+    }
 };
 
-#endif
+#endif // ROAD_REMOVED_H
