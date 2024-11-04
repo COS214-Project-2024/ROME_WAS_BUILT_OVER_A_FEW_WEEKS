a710400f3c70cde197983e3d2135a5d90a4fcf9e
mutarisi <147860027+mutarisi@users.noreply.github.com>
2024-11-04 22:33:04 +0200

    Add files via upload

--- System/BoosterStructures/Railway.h
+++ System/BoosterStructures/Railway.h
@@ -3,10 +3,21 @@
 
 #include "BoosterStructures.h"
 
-class Railway: public BoosterStructures{
-    public:
-        Railway();
-
+/**
+ * @class Railway
+ * @brief Represents a Railway structure that provides satisfaction boosts to the surrounding areas.
+ *
+ * Railway is a derived class of BoosterStructures and represents a railway system in the simulation,
+ * which can provide specific enhancements to nearby structures or areas.
+ */
+class Railway : public BoosterStructures {
+public:
+    /**
+     * @brief Constructs a Railway object.
+     *
+     * Initializes a new instance of the Railway class with default values.
+     */
+    Railway();
 };
 
-#endif
+#endif // RAILWAY_H
